c9bc4f0387eae7c729f7a872b40e2e929421e412
vCoutinh0 <vini.coutinho.if@gmail.com>
2024-08-07 16:09:14 -0300

    Merge projeto calculadora e implementação das operações restante da calculadora

---
+++ Application/Operacoes.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Application
+{
+    public class Operacoes
+    {
+        public static float Dividir(float n1, float n2)
+        {
+            if (n2 == 0)
+                throw new DivideByZeroException("Não é possível dividir por zero");
+
+            return n1 / n2;
+        }
+
+        public static float Multiplicar(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
